19297480e3055eb6b97560440da0a569b1a1e38a
Matheo Delaunay <matheo.delaunay@adeo.com>
2023-05-04 15:35:10 +0200

    matheo

--- app/blockchain-front/src/components/VotingComponent.vue
+++ app/blockchain-front/src/components/VotingComponent.vue
@@ -1,12 +1,12 @@
 <script setup lang="ts">
 import Web3 from "web3";
+import abi from '../../../../back/build/contracts/Voting.json'
 
-const fs = require('fs')
-const contract = JSON.parse(fs.readFileSync("../../../../back/build/contracts/Voting.json"))
-console.log(contract)
-
-const web3 = new Web3('http://localhost:8545');
+const web3 = new Web3('http://localhost:7545');
 const contractAddress = "0xEDE4F40fADb30cD0DD7f04B43422175086b35121"
+console.log(abi.abi)
+const contract = new web3.eth.Contract(abi.abi, contractAddress);
+console.log(contract)
 </script>
 
 <template>
