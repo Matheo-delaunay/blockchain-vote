1bb3f17ab14db477e476037c927edc1eaac04c4e
Nathan Audvard <nathan.audvard@gmail.com>
2023-05-05 21:54:36 +0200

    wip

---
+++ app/blockchain-front/src/components/StartVote.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="container">
+    <div class="row justify-content-center">
+        <button class="btn btn-primary" v-on:click="startVote">Start vote</button>
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
