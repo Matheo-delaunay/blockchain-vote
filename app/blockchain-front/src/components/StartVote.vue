c25bf1c0184e2d9e1be2b3fae0eba2311e3a99af
nathan-audv <58268291+nathan-audv@users.noreply.github.com>
2023-05-05 23:18:59 +0200

    feat: admin start vote (#15)

---
+++ app/blockchain-front/src/components/StartVote.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+const startVote = () => {
+
+}
+</script>
+
+<template>
+<div class="container">
+    <div class="row justify-content-center">
+        <button class="btn btn-primary" v-on:click="startVote">Start vote</button>
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
