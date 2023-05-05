03b7f774b46f7c1cebcb1cc005661aa52d12ea0e
Nathan Audvard <nathan.audvard@gmail.com>
2023-05-05 22:25:23 +0200

    wip

---
+++ app/blockchain-front/src/components/AddProposal.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="row my-4">
+            <div class="col-8">
+                <label for="addedProposal" class="form-label">Make a proposal</label>
+                <input class="form-control" type="text" id="addedProposal"/>
+            </div>
+            <div class="col-4 d-grid my-auto">
+                <button class="btn btn-primary btn-block" id="buttonAdd" v-on:click="add">Submit proposal
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
