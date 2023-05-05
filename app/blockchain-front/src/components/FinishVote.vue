0cfa4d0c535161223f32a94115a04b972efb5c56
Nathan Audvard <nathan.audvard@gmail.com>
2023-05-05 22:14:46 +0200

    wip

---
+++ app/blockchain-front/src/components/FinishVote.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <button class="btn btn-primary" v-on:click="finishVote">FinishVote</button>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
