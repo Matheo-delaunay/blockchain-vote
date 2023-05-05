74e237bf13fd746137b728ee1b294bbd9aedef62
Nathan Audvard <nathan.audvard@gmail.com>
2023-05-05 23:16:08 +0200

    wip

--- app/blockchain-front/src/components/AddProposal.vue
+++ app/blockchain-front/src/components/AddProposal.vue
@@ -1,5 +1,8 @@
 <script setup lang="ts">
 
+const addProposal = () => {
+
+}
 </script>
 
 <template>
@@ -10,7 +13,7 @@
                 <input class="form-control" type="text" id="addedProposal"/>
             </div>
             <div class="col-4 d-grid my-auto">
-                <button class="btn btn-primary btn-block" id="buttonAdd" v-on:click="add">Submit proposal
+                <button class="btn btn-primary btn-block" id="buttonAdd" v-on:click="addProposal">Submit proposal
                 </button>
             </div>
         </div>
