01eecaaec5f1b2fd2f7497567364c58bced0700b
nathan-audv <58268291+nathan-audv@users.noreply.github.com>
2023-05-05 23:16:56 +0200

    feat: user add proposal (#18)

---
+++ app/blockchain-front/src/components/AddProposal.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+
+const addProposal = () => {
+
+}
+</script>
+
+<template>
+    <div class="container">
+        <div class="row my-4">
+            <div class="col-8">
+                <label for="addedProposal" class="form-label">Make a proposal</label>
+                <input class="form-control" type="text" id="addedProposal"/>
+            </div>
+            <div class="col-4 d-grid my-auto">
+                <button class="btn btn-primary btn-block" id="buttonAdd" v-on:click="addProposal">Submit proposal
+                </button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
