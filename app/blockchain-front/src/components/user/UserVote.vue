16c3f52acfaa665644c4936c0cc6b19078bb7d70
Nathan Audvard <nathan.audvard@gmail.com>
2023-05-06 03:18:51 +0200

    wip

---
+++ app/blockchain-front/src/components/user/UserVote.vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import {reactive} from "vue";
+
+// Utiliser la méthode de ShowProposal pour afficher un tableau avec description du vote et bouton pour voter
+const rowData: string[] = []
+const reactiveData = reactive(rowData)
+
+const vote = (proposalId: number) => {
+
+}
+</script>
+
+<template>
+<div class="container">
+    <div class="row">
+        <table class="table" id="myTable">
+            <tr v-for="item in reactiveData">
+                <th style="width: 80%;" scope="row" :id="item">{{ item }}</th>
+                <td style="width: 20%;"><button class="btn btn-outline-danger " v-on:click="vote(reactiveData.indexOf(item))">Vote</button></td>
+            </tr>
+        </table>
+    </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
